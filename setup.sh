fd862c10129de9c0478b5e910eb634579f8c111e
HMSBennett <harrisonsbennett@gmail.com>
2019-08-01 10:11:04 +0000

    gimmeyours doesn't work and has been removed

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 echo "function gimme() {"  >> ~/.bashrc
 echo "    git clone https://github.com/HMSBennett/$1" >> ~/.bashrc
 echo "}" >> ~/.bashrc
+
 echo "" >> ~/.bashrc
-echo "function gimmeyours() {"  >> ~/.bashrc
-echo "    git clone https://github.com/$1/$2" >> ~/.bashrc
-echo "}" >> ~/.bashrc
+
+export gimme
+
+source ~/.bashrc

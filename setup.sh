e3eb24f7f1f02089e9cac2ee6945a04b70806c8d
Harrison Bennett <32133987+HMSBennett@users.noreply.github.com>
2019-08-01 11:05:56 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+echo "function gimme() {"  >> ~/.bashrc
+echo "    git clone https://github.com/HMSBennett/$1" >> ~/.bashrc
+echo "}" >> ~/.bashrc
+echo "" >> ~/.bashrc
+echo "function gimmeyours() {"  >> ~/.bashrc
+echo "    git clone https://github.com/$1/$2" >> ~/.bashrc
+echo "}" >> ~/.bashrc

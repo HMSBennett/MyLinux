17253435c56b8d378e0eefffa131ea5ddf010839
HMSBennett <harrisonsbennett@gmail.com>
2019-08-04 20:27:01 +0000

    Updated setup to include git config

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 echo "function gimme() {"  >> ~/.bashrc
-echo "    git clone https://github.com/HMSBennett/$1" >> ~/.bashrc
+echo "    git clone https://github.com/HMSBennett/\$1" >> ~/.bashrc
 echo "}" >> ~/.bashrc
 
 echo "" >> ~/.bashrc
@@ -7,3 +7,43 @@ echo "" >> ~/.bashrc
 export gimme
 
 source ~/.bashrc
+#-------------------------------------------------------------
+
+git config --global user.email "harrisonsbennett@gmail.com"
+git config --global user.name "HMSBennett"
+
+#-------------------------------------------------------------
+
+git clone https://github.com/bob-crutchley/vim
+
+cd vim
+
+./install
+
+cd
+
+#-------------------------------------------------------------
+
+git clone https://github.com/HMSBennett/DockerInstall
+
+cd DockerInstall
+
+./install.sh
+
+./docCompInstall.sh
+
+cd
+
+#-------------------------------------------------------------
+
+git clone https://github.com/HMSBennett/Terraform
+
+cd Terraform
+
+./install.sh
+
+cd
+
+#-------------------------------------------------------------
+
+rm -rf Terraform DockerInstall vim
